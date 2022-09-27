77928e13ae65613882b29f0a76dd6c6192984bd3
Andre Monteiro <monteiro.hat@gmail.com>
2022-09-27 20:25:22 -0300

    words reservation

--- study/words_reserved.py
+++ study/words_reserved.py
@@ -1,4 +1,42 @@
 
+dict_words = {
+    'False': "It is a boolean operator that represents the opposite of True.",
+    'None': "There is no null value in Python. The None is an object that represents the absence of a value. It is like an empty object.",
+    'True': "It is a boolean operator that represents if the value is True",
+    'and': "It is one of the logical operators that returns True if both of the statements are True. Check The Truth table",
+    'as': "The as statement in Python re-assigns a returned object to a new identifier. Basically, it creates an alias.",
+    'assert': "The assert statement in Python is used for debugging.",
+    'break': "It is a control flow statement used to come out of a loop.",
+    'class': "The class keyword is udes to define a class in Python.",
+    'continue': "It is a control flow statement used to continue to the next iteration of a loop. Unlike break, the continue statement does not exit the loop.",
+    'def': "The def function is used to define a function or a method in Python.",
+    'del': "The del statement is used to delete an object in Python.",
+    'elif': "Shorthand for else if, checks if some other condition holds when the condition in the if statement is false.",
+    'else': "Conditional statement that tells to perform alternate action if the condition in the if statement is False.",
+    'except': "Part of the type… except errorhandling structure in Python. Tells what to do when an exception occurs.",
+    'finally': "The finally statement is an optional part of try… except error. It always executes the code regardless of whether an error was thrown or not.",
+    'for': "The keyword is used to create a for loop.",
+    'from': "This statement is used when you can to include a specific part of the module.",
+    'global': "Accessing a global variable is simple as any other variable but to modify a global variable, you need to use the global keyword.",
+    'if': "An if statement is used to make a conditional statement. If the condition is True, then some action is performed.",
+    'import': "This statement is used to import modules to the project.",
+    'in': "The in statement is used to check if an element is present in an iterable like list or tuple.",
+    'is': "This statement is used to check if the two variables are equal.",
+    'lambda': "A lambda function in Python is an anonymous function. It can take any number of arguments but only have a single expression.",
+    'nonlocal': "This keyword is used in functions inside functions to create anonymous functions.",
+    'not': "It is another logical operator that returns False when the value is True and vice versa. Check the truth table.",
+    'or': "It is a logical operator that returns True if any one of the statements is True. Check the truth table.",
+    'pass': "It is a null statement in Python that will do nothing.",
+    'raise': "The raise statement is used to raise an error. These errors are visible in the traceback and they cancel the execution of the program if not handled properly.",
+    'return': "This keyword is used to exit a function or a method and return some value.",
+    'try': "The try statement is used to make a try… except statement. The try statement starts a block of code that is tried to execute. If it fails, the except block catches the error.",
+    'while': "This statement is used to start a while loop. It continues iteration until a condition is no longer True.",
+    'with': "The with statement is used to simplify the exception handling.",
+    'yield': "The yield function ends a function and returns an iterator."
+
+}
+
 
 def print_words_reserved():
-   print(['False', 'None', 'True', 'and', 'as', 'assert', 'break', 'class', 'continue', 'def', 'del', 'elif', 'else', 'except', 'finally', 'for', 'from', 'global', 'if', 'import', 'in', 'is', 'lambda', 'nonlocal', 'not', 'or', 'pass', 'raise', 'return', 'try', 'while', 'with', 'yield'])
+    for word in dict_words.items:
+      print(word.)
